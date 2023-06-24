b30c6d7eea4260fd0e0a830c5e98c219d6c45869
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-06-24 12:38:30 +0530

    logout redirects to login page

--- TaskNet/settings.py
+++ TaskNet/settings.py
@@ -138,7 +138,7 @@ AUTH_USER_MODEL = 'core.CustomUser'
 # Redirect after login
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/index'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 # Attach Files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
