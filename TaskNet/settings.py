a9bc97813b4ea247b21ac7f61c2fe329708a2a8d
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-07-03 13:45:07 +0530

    updated settings.py MIDDLEWARE

--- TaskNet/settings.py
+++ TaskNet/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.compress.WhitenoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddlewar'
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
