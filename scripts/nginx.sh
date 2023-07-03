5c0d51d9202f051b3b5d87f3a41f88022d734542
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-07-03 14:47:01 +0530

    updated ngnix.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,8 @@ sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/TaskNet/nginx/nginx.conf /etc/nginx/sites-available/TaskNet
-sudo ln -s /etc/nginx/sites-available/TaskNet /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/TaskNet /etc/nginx/sites-enabled
-#sudo nginx -t
-sudo gpasswd -a www-data ubuntu
+sudo ln -s /etc/nginx/sites-available/TaskNet /etc/nginx/sites-enabled
+sudo nginx -t
 sudo systemctl restart nginx
+sudo ufw delete allow 8000
+sudo ufw allow 'Nginx Full'
