f267a15e688dfb3fbe0163b177681dbc095946f8
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-07-03 16:15:03 +0530

    updated python_dependencies

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -2,6 +2,7 @@
 
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
-sudo apt-get install pkg-config
+sudo apt-get update
+sudo apt-get install -y libmysqlclient-dev
 pip install mysqlclient
 pip install -r /home/ubuntu/TaskNet/requirements.txt
