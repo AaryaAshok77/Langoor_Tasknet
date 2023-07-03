fda7b0b164c10f0656df3ee40a14cea964f5bf3b
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-07-03 13:53:30 +0530

    updated nginx wrong file path

--- TaskNet/settings.py
+++ TaskNet/settings.py
@@ -127,8 +127,6 @@ STATIC_ROOT = '/Users/aaryaashokk/Documents/Coding/VS Code/Langoor/TaskNet/stati
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
