0fe6b772427037956191c94cc36ccb6d9b6e42fb
Aarya Ashok K <aaryaashokk77@gmail.com>
2023-07-03 13:57:31 +0530

    updated settings.py MIDDLEWARE

--- TaskNet/settings.py
+++ TaskNet/settings.py
@@ -127,6 +127,8 @@ STATIC_ROOT = '/Users/aaryaashokk/Documents/Coding/VS Code/Langoor/TaskNet/stati
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
